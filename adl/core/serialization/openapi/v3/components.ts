bb12c9da8f0a252c1a0f116de5a50d6b3726c4cb
Garrett Serack <garretts@microsoft.com>
2020-05-08 11:04:54 -0700

    cleanup

--- adl/core/serialization/openapi/v3/components.ts
+++ adl/core/serialization/openapi/v3/components.ts
@@ -14,7 +14,7 @@ const { vendorExtensions } = v3;
 
 
 // eslint-disable-next-line require-yield
-export async function *processComponents(components: v3.Components, $: Context): AsyncGenerator<Element> {
+export async function* processComponents(components: v3.Components, $: Context): AsyncGenerator<Element> {
 
   for (const [key, extension] of vendorExtensions(components)) {
     // switch(key)
@@ -23,15 +23,15 @@ export async function *processComponents(components: v3.Components, $: Context):
 
   // definitely, schemas first, since so much will $ref them
   // await consume($.process(processSchemas, components.schemas));
-  for await (const schema of $.processDictionary(processSchema, components.schemas) ) {
+  for await (const schema of $.processDictionary(processSchema, components.schemas)) {
     // we have to split up where schemas go 
-    if( schema instanceof A) {
+    if (schema instanceof A) {
       throw new Error('schemas have their own alias type');
     }
-    if( schema instanceof Alias) {
+    if (schema instanceof Alias) {
       $.api.schemas.aliases.push(schema);
       continue;
-    } 
+    }
     if (schema instanceof ObjectSchema) {
       $.api.schemas.objects.push(schema);
       continue;
@@ -39,46 +39,46 @@ export async function *processComponents(components: v3.Components, $: Context):
     if (schema instanceof Enum) {
       $.api.schemas.enums.push(schema);
       continue;
-    } 
+    }
     if (schema instanceof Constant) {
       $.api.schemas.constants.push(schema);
       continue;
     }
-    if (schema instanceof AndSchema || schema instanceof XorSchema || schema instanceof AnyOfSchema ) {
+    if (schema instanceof AndSchema || schema instanceof XorSchema || schema instanceof AnyOfSchema) {
       $.api.schemas.combinations.push(schema);
       continue;
     }
-    if(schema instanceof AnySchema || schema instanceof ArraySchema || schema instanceof Primitive) {
+    if (schema instanceof AnySchema || schema instanceof ArraySchema || schema instanceof Primitive) {
       continue;
     }
-    
+
     throw new Error('Should not get here.');
   }
-  
+
   // if there are vendor extensions in the dictionary, they should be handled like this:
   // for (const { key, value: extension } of vendorExtensions(components.headers)) {
   // ... do something ...
   // }
-  for await( const h  of $.processDictionary(header, components.headers) ) {
+  for await (const h of $.processDictionary(header, components.headers)) {
     $.api.http.headers.push(h);
-  } 
+  }
 
   for await (const p of $.processDictionary(parameter, components.parameters)) {
     $.api.http.parameters.push(p);
   }
-  
-  for await( const request of $.processDictionary(requestBody, components.requestBodies)) {
-    $.api.http.requests.push( request);
+
+  for await (const request of $.processDictionary(requestBody, components.requestBodies)) {
+    $.api.http.requests.push(request);
   }
-  
+
   for await (const rsp of $.processDictionary(response, components.responses)) {
     $.api.http.responses.push(rsp);
   }
-  
+
   for await (const auth of $.processDictionary(securityScheme, components.securitySchemes)) {
     $.api.http.authentications.push(auth);
   }
-  
+
 
   // await $.process(processExamples, components.examples); // send to attic 
   // await $.process(processCallbacks, components.callbacks);  // ok skip
--- adl/core/serialization/openapi/v3/response.ts
+++ adl/core/serialization/openapi/v3/response.ts
@@ -8,12 +8,12 @@ import { processInline } from './schema';
 import { Context } from './serializer';
 
 
-export async function *response(response: v3.Response, $: Context, options?: { isAnonymous?: boolean }): AsyncGenerator<Response> {
-  const responseName  = options?.isAnonymous ? anonymous('response') : nameOf(response);
-  
-  if( length(response.content) === 0 ) {
+export async function* response(response: v3.Response, $: Context, options?: { isAnonymous?: boolean }): AsyncGenerator<Response> {
+  const responseName = options?.isAnonymous ? anonymous('response') : nameOf(response);
+
+  if (length(response.content) === 0) {
     // no body/content
-    const result = new Response(responseName,'', {
+    const result = new Response(responseName, '', {
       description: response.description,
     });
 
@@ -22,11 +22,11 @@ export async function *response(response: v3.Response, $: Context, options?: { i
         result.headers.push(h);
       }
     }
-    return yield addExtensionsToAttic( result, response);
+    return yield addExtensionsToAttic(result, response);
   }
-  
-  for (const [ mediaType, type]  of items(response.content)) {
-    const result = new Response(responseName,mediaType, {
+
+  for (const [mediaType, type] of items(response.content)) {
+    const result = new Response(responseName, mediaType, {
       description: response.description,
       schema: await firstOrDefault(processInline(type.schema, $)),
     });
@@ -36,7 +36,7 @@ export async function *response(response: v3.Response, $: Context, options?: { i
         result.headers.push(h);
       }
     }
-      
+
     // example data we can figure out later.
     result.addToAttic('example', type.example);
     // example data we can figure out later.
