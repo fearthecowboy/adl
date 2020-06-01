2c29e86e943677e99f654a9177dc8c7a5e191f16
Garrett Serack <garretts@microsoft.com>
2020-06-01 09:11:57 -0700

    sample compiles

--- adl/sample-plugin/exports.ts
+++ adl/sample-plugin/exports.ts
@@ -1,12 +1,45 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
-// exports 
-const items: any = {}
-for (const each of fs.readdirSync()) {
-  // .js files
-  items[name] = require(`./${each}`);
+// automatically pull exports from all .js files in this project that 
+// have a default export
+function addExports(location: string = __dirname): any | undefined {
+  const result: any = {};
+  let count = 0;
+  for (const name of fs.readdirSync(location)) {
+    const fullPath = path.join(location, name);
+    const stat = fs.statSync(fullPath);
+
+    // directories should be recursed
+    if (stat.isDirectory()) {
+      const subdir = addExports(fullPath);
+      if (subdir) {
+        result[name] = subdir;
+      }
+      continue;
+    }
+
+    // .js files are checked for default exports
+    // and added to the map 
+    if (name.endsWith('.js')) {
+      try {
+        const mod = require(fullPath);
+        if (mod.default) {
+          // files with a default export are added to the tree.
+          result[name.replace(/\.js$/g, '')];
+        }
+        count++;
+      } catch {
+        // ignore files that don't import cleanly. 
+      }
+    }
+  }
+  return count ? result : undefined;
 }
-export default items;
+
+const plugins = addExports();
+export default plugins;
+
 /*
 export default <plugins>{
   sample,
